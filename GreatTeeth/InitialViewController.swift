1661e4463237094230c4e8f9f92802c777e67ea9
Aleksander Skjoelsvik <alekplay@hotmail.com>
2016-04-21 21:34:21 -0400

    Initial commit

---
+++ GreatTeeth/InitialViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.swift
+//  GreatTeeth
+//
+//  Created by Aleksander Skjoelsvik on 4/21/16.
+//  Copyright © 2016 Aleksander Skjoelsvik. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+}
