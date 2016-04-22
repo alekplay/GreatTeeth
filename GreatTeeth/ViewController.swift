12c7e16d919c862b85b7cfc8d03e0fadccdf55bf
Aleksander Skjoelsvik <alekplay@hotmail.com>
2016-04-21 21:28:33 -0400

    Initial Commit

---
+++ GreatTeeth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreatTeeth
+//
+//  Created by Aleksander Skjoelsvik on 4/21/16.
+//  Copyright © 2016 Aleksander Skjoelsvik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
