0d2377ee62cb5db2f8391940322072c4110c1bfa
Aleksander Skjoelsvik <alekplay@hotmail.com>
2016-04-21 22:09:23 -0400

    Added background gradient for main view

--- GreatTeeth/InitialViewController.swift
+++ GreatTeeth/InitialViewController.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class InitialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add background gradient
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        let color1 = UIColor(red:45/255.0, green:179/255.0, blue:183/255.0, alpha:1.0).CGColor as CGColorRef
+        let color2 = UIColor(red:32/255.0, green:107/255.0, blue:133/255.0, alpha:1.0).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2]
+        gradientLayer.locations = [0.0, 1.0]
+        view.layer.addSublayer(gradientLayer)
+        
+        // Remove navigation bar border
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+    }
 
 }
